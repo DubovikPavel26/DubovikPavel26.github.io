954badc1bd303d8b7cd59c665b77721c512bcc04
pavel <dubovikdmc@gmail.com>
2022-08-04 14:10:21 +0300

    Урок 1

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let storeName = 'statibo';
+let storeDescription = {budget: 10000, employees: ['pasha', 'slava', 'dima'], products: {cucumber: 200, potato: 300}, open: true};
+
+alert('Hello');
+
+const resault = confirm("Are you here&");
+
+const answer = prompt("Вам есть 18?", "");
+
+const answers = []
+
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваша фамилия?", "");
+answers[2] = prompt("Как ваше отчество?", "");
+
+
+const category = "toys";
+
+console.log(`i like${category}`);
